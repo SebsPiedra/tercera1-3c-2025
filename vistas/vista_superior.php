e398c84da91b081bcdad2c688aa6c45bf9746340
Sebas <sebaspiedra23@gmail.com>
2025-10-15 18:21:22 -0600

    Creación de proyecto

---
+++ vistas/vista_superior.php
@@ -0,0 +1,11 @@
+ <!DOCTYPE html>
+ <html lang="spanish">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Tercera 1" content="Proyecto Universitario"> 
+    <title>Tercera 1</title>
+    <link rel="stylesheet" href="./css/main.css">
+
+
+</head>
